b926bfe2b9599d35f2fbecfa93c642087c345bc2
Guillaume Emont <guijemont@igalia.com>
2021-12-15 20:50:29 +0000

    scikit-optimize: added more options

--- scikit-optimize-js2.py
+++ scikit-optimize-js2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S python3 -u
 
 # FIXME: logging
-# FIXME: output log/graphs in output dir (default pwd, with command-line option)
-from skopt import gp_minimize
+# FIXME: save detailed results
+from skopt import gp_minimize, dump
 
 from scipy.stats import gmean, tmean, tvar
 import numpy as np
@@ -12,6 +12,7 @@ import argparse
 from collections import namedtuple
 from skopt.plots import plot_convergence, plot_objective
 from datetime import datetime
+from pathlib import Path
 
 Parameter = namedtuple('Parameter', ('name', 'range', 'default'))
 parameters = (Parameter('maximumFunctionForCallInlineCandidateBytecodeCost', (0, 200), 120),
@@ -28,8 +29,17 @@ parser.add_argument('-j', '--jsc-path', required=True,
                     help="path of jsc executable on remote host")
 parser.add_argument('--initial-point-generator', default="random",
                     help="path of jsc executable on remote host")
-#parser.add_argument('-n', '--iteration-per-config', type=int, default=1,
-#                    help="how many times to run JetStream2 for each tested configuration")
+parser.add_argument('-n', '--n-calls', type=int, default=50,
+                    help="how many times to run JetStream2")
+parser.add_argument('--initial-points', type=int, default=10,
+                    help="how many random points to evaluate before using estimator")
+parser.add_argument('-p', '--pre-run', type=int, default=5,
+                    help="How many times to initially run JetStream2 to calculate variance")
+parser.add_argument('-o', '--output-dir', type=str, default="./",
+                    help="Where to output everything we generate")
+parser.add_argument('-g', '--dump-graphs', action='store_true',
+                    help="Save graphics")
+
 
 def score_from_json(res):
     l = [test['metrics']['Score']['current'][0] for test in res['JetStream2.0']['tests'].values()]
@@ -68,41 +78,53 @@ def get_optimization_func(options):
         score = run_jetstream2(options.remote, options.ssh_id, options.jsc_path, env=env)
         print(f"optimize_me({arguments}) → {score}")
         return -score
-    
+
     return optimize_me
 
 
 def default_preruns(options):
     env = dict((p.name, p.default) for p in parameters)
-    scores = [run_jetstream2(options.remote, options.ssh_id, options.jsc_path, env=env) for _ in range(5)]
+    scores = [run_jetstream2(options.remote, options.ssh_id, options.jsc_path, env=env) for _ in range(options.pre_run)]
     return (tmean(scores), tvar(scores))
 
-def saveGraphs(res):
+def save_results(options, output_dir, res):
     nowStr = datetime.now().strftime('%Y-%m-%d-%H%M%S')
-    fig = plot_convergence(res).get_figure()
-    fig.savefig(f"{nowStr}-convergence.png")
-    fig = plot_objective(res, dimensions=[p.name for p in parameters])[0][0].get_figure()
-    fig.savefig(f"{nowStr}-objective.png")
+    if options.dump_graphs:
+        fig = plot_convergence(res).get_figure()
+        fig.savefig(output_dir / f"{nowStr}-convergence.png")
+        fig = plot_objective(res, dimensions=[p.name for p in parameters])[0][0].get_figure()
+        fig.savefig(output_dir / f"{nowStr}-objective.png")
+    dump(res, output_dir / f"{nowStr}-dump.pkl", store_objective=False)
+
+
+def prepare_output(options):
+    output_dir = Path(options.output_dir)
+    output_dir.mkdir(exist_ok=True)
+    return output_dir
 
 if __name__ == '__main__':
     options = parser.parse_args()
     print(f"options: {options}")
-    y0, noise = default_preruns(options)
-    print(f"With defaults ({dict((p.name, p.default) for p in parameters)}): result: {y0}, variance: {noise}")
+    output_dir = prepare_output(options)
+    gp_minimize_kargs = {}
+    if options.pre_run >=3:
+        y0, noise = default_preruns(options)
+        print(f"With defaults ({dict((p.name, p.default) for p in parameters)}): result: {y0}, variance: {noise}")
+        gp_minimize_kargs['y0'] = -y0
+        gp_minimize_kargs['noise'] = noise
+        gp_minimize_kargs['x0'] = [p.default for p in parameters]
+
     func = get_optimization_func(options)
     import time
     print ("Starting to minimize")
     before = time.monotonic()
     res = gp_minimize(func,
                       [p.range for p in parameters],
-                      n_calls=50,
-                      x0=[p.default for p in parameters],
-                      y0=-y0,
-                      noise=noise,
+                      n_calls=options.n_calls,
+                      n_initial_points=options.initial_points,
                       initial_point_generator=options.initial_point_generator,
-                      verbose=True)
+                      verbose=True,
+                      **gp_minimize_kargs)
     print(f"gp_minimize ran in {time.monotonic() - before}s")
     print(f"best: {res.x} → {-res.fun}")
-    saveGraphs(res)
-    axes = plot_convergence(res)
-    
+    save_results(options, output_dir, res)
